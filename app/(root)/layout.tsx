ff1afb79e5185c2f25e98cfd84e3368e1e2dafb6
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-04-28 18:30:55 -0400

    setup project layout & folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layout = ( {children} : {
+    children: React.ReactNode
+}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
